2705123f06d46f8fdd1989629a85a5d915c53bd4
Azamat <azamattolegenov1gmail.com>
2020-12-04 22:20:13 +0600

    Second task(completed)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 "use strict";
 
+// First task
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 console.log(numberOfFilms);
+
+//Second task
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actord: {},
+    genres: [],
+    privat: false
+};
